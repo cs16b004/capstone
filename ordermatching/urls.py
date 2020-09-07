63678c9cd4713e44d839137e9e7ecce81f76478c
muvvasandeep_cs16b017 <35159794+muvvasandeep@users.noreply.github.com>
2020-09-07 15:27:52 +0530

    Updated Home page

--- ordermatching/urls.py
+++ ordermatching/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home-page'),
+    path('signup/', views.signup, name = 'signup-page'),
+    path('signin/', views.signin, name = 'signin-page'),
 ]
--- ordermatching/views.py
+++ ordermatching/views.py
@@ -2,3 +2,9 @@ from django.shortcuts import render
 
 def home(request):
 	return render(request, 'home/home.html')
+
+def signin(request):
+	return render(request, 'signin/signin.html')
+
+def signup(request):
+	return render(request, 'signup/signup.html')
