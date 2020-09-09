b7e99a4bdd7fd56d857a3a9ed147b4b2572195d3
BodapatiNirupamasai <35302469+BodapatiNirupamasai@users.noreply.github.com>
2020-09-09 14:16:24 +0530

    Update views.py

--- ordermatching/views.py
+++ ordermatching/views.py
@@ -1,13 +1,52 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from ordermatching.models import UserSignup
+from django.template import loader
+from django.http import HttpResponse
 
 def home(request):
 	return render(request, 'home/home.html')
 
-def signin(request):
-	return render(request, 'signin/signin.html')
+def tradeView(request):
+    username = ""
+    if request.session.has_key('username'):
+      username = request.session['username']
+    return render(request,'trade.html',{'username' : username})
 
-def signup(request):
-	return render(request, 'signup/signup.html')
+def signinView(request):
+    if request.method == 'POST':
+        user_name = request.POST['username']
+        pwd = request.POST['password']
+        request.session['username'] = user_name
+        user_obj = UserSignup.objects.filter(username = user_name, password=pwd).exists()
+        if user_obj :
+            print("user object ecists")
+            return render(request,'trade.html')
+        else :
+            return render(request,'/signup/signup.html')
+    else:
+        return render(request, 'signin/signin.html')
+
+
+
+def signupView(request):
+    if request.method == "POST":
+        user_name = request.POST['username']
+        first_name = request.POST['firstname']
+        last_name = request.POST['lastname']
+        pwd = request.POST['password']
+
+        request.session['username'] = user_name
+
+        if UserSignup.objects.filter(username=user_name).exists():
+            print("Username Taken") # use toolkit to show it there(js, css etc)
+        else :
+            user1 = UserSignup.objects.create(username = user_name,firstname=first_name,lastname=last_name,password=pwd)
+            user1.save()
+            print("User Created")
+            return render(request,'trade.html')
+    else :
+        return render(request,'signup/signup.html')
+
 
 def accepted(request):
 	return render(request, 'tradeslist/accepted/accepted.html')
