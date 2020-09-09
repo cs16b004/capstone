c04a6f0889bfd96f84c91094fec2af152ead9609
BodapatiNirupamasai <35302469+BodapatiNirupamasai@users.noreply.github.com>
2020-09-09 14:17:12 +0530

    Update models.py

--- ordermatching/models.py
+++ ordermatching/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+
+class UserSignup(models.Model):
+    username = models.CharField(max_length=20)
+    firstname = models.CharField(max_length=20)
+    lastname = models.CharField(max_length=20)
+    password = models.TextField(max_length=10)
