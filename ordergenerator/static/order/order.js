d63cd8a81a70359896d812c38921acc32dad9296
muvvasandeep_cs16b017 <35159794+muvvasandeep@users.noreply.github.com>
2020-09-10 16:31:30 +0530

    Uploaded static files

---
+++ ordergenerator/static/order/order.js
@@ -0,0 +1,34 @@
+$("#id_order_type").change(function(){
+	alert("k")
+	if ($(this).val() == "MR") {
+
+		$("#id_all_or_none").prop("checked",true)
+		$("#id_all_or_none").prop("disabled",true)
+		$('#id_Minimum_fill').prop('disabled', true);
+		$('#id_Disclosed_Quantity').prop('disabled', true);
+	}
+	else {
+		$("#id_all_or_none").prop("disabled",false)
+		$("#id_all_or_none").prop("checked",false)
+		$('#id_Minimum_fill').prop('disabled', false);
+		$('#id_Disclosed_Quantity').prop('disabled', false);
+	}
+})
+
+$("#id_all_or_none").change(function(){
+	alert("ok")
+	if ($(this).is(':checked')){
+		$('#id_Minimum_fill').prop('disabled', true);
+		$('#id_Disclosed_Quantity').prop('disabled', true);
+	}
+	else {
+		$('#id_Minimum_fill').prop('disabled', false);
+		$('#id_Disclosed_Quantity').prop('disabled', false);
+	}
+})
+
+$( function () {
+	$("#myModal").draggable({
+      handle: ".modal-header"
+	}); 
+});
